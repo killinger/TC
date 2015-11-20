b443c7dcc85f08215c5cda531d0e284a3501f897
killinger <hnrk.prsn@gmail.com>
2015-11-20 10:19:30 +0100

    allt

---
+++ SPA/SPA/www/scripts/controllers.js
@@ -0,0 +1,30 @@
+﻿var app = angular.module('TeleCoaching', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+
+      .when('/', {
+          templateUrl: 'main.html',
+          controller: 'MainController'
+
+      })
+
+    .when('/second', {
+        templateUrl: 'second.html',
+        controller: 'SecondController'
+    })
+
+});
+
+app.controller('MainController', ['$scope', function ($scope) {
+
+}]);
+
+app.controller('SecondController', ['$scope', function ($scope) {
+    $scope.myObj = {
+        ett: { 'state': 'locked' },
+        två: { 'state': 'available' },
+        tre: { 'state': 'locked' }
+    };
+    document.getElementById("knapp2").addEventListener("click", test2);
+}]);
---
+++ SPA/SPA/www/scripts/script.js
@@ -0,0 +1,9 @@
+﻿var apa = 1;
+function test() {
+    apa = 2;
+    alert(apa);
+};
+
+function test2() {
+    alert(apa);
+};
