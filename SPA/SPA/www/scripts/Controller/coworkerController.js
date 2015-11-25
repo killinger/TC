daaeb07faf542e5dcf06f7ca43d098f0595a213a
Deepeeper <albin.pettersson@hotmail.com>
2015-11-25 11:21:40 +0100

    coworks

--- SPA/SPA/www/scripts/Controller/coworkerController.js
+++ SPA/SPA/www/scripts/Controller/coworkerController.js
@@ -5,12 +5,17 @@
         tre: { 'state': 'locked' }
     };
 
-    $scope.test = true;
+    $scope.cardsDone = 2;
 
-    $scope.dateFromFile = new Date();
+    var dateFromFile = new Date("12/31/2015");
+    var currentDate = new Date("1/1/2016");
 
     $scope.checkDate = function(){
-
+        console.log(dateFromFile);
+        console.log(currentDate);
+        var timeDiff = Math.abs(currentDate.getTime() - dateFromFile.getTime());
+        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        return diffDays;
     };
 
     $scope.readFiles = function(){
