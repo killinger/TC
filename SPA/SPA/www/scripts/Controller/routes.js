3ea0440b5b96ccf4dc1198e6091fb8cd38a39c56
Deepeeper <albin.pettersson@hotmail.com>
2015-11-25 09:34:40 +0100

    med router

---
+++ SPA/SPA/www/scripts/Controller/routes.js
@@ -0,0 +1,22 @@
+﻿
+app.config(function ($routeProvider) {
+    $routeProvider
+
+      .when('/', {
+          templateUrl: 'PartialViews/main.html',
+          controller: 'mainController'
+
+      })
+
+     .when('/leader', {
+          templateUrl: 'PartialViews/leader.html',
+          controller: 'leaderController'
+
+     })
+
+    .when('/coworker', {
+        templateUrl: 'PartialViews/coworker.html',
+        controller: 'coworkerController'
+    })
+});
+
